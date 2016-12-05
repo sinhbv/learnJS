426c17194d7f4f39ced3bbfd8e03ca5a9208bbd0
Nick Gao <sinhbv@gmail.com>
2016-12-05 11:19:03 +0800

    add the sepc file for the remove middle item from an array

--- spec/removeMidItemFromArray-spec.js
+++ spec/removeMidItemFromArray-spec.js
@@ -5,4 +5,24 @@ describe('移除一个数组中位于中间的元素', function(){
         var result = removeMidItemFromArray();
         expect(result).toBe(undefined);
     });
+    it('给定一个空数组的情况', function(){
+        var result = removeMidItemFromArray([]);
+        expect(result).toEqual([]);
+    });
+    it('给定数组中只有一个元素的情况', function(){
+        var result = removeMidItemFromArray([1]);
+        expect(result).toEqual([]);
+    });
+    it('给定数组中有两个元素的情况', function(){
+        var result = removeMidItemFromArray([1,2]);
+        expect(result).toEqual([]);
+    });
+    it('给定数组中有三个元素的情况', function(){
+        var result = removeMidItemFromArray([1,2,3]);
+        expect(result).toEqual([1,3]);
+    });
+    it('给定数组中有四个元素的情况', function(){
+        var result = removeMidItemFromArray([1,2,3,4]);
+        expect(result).toEqual([1,4]);
+    });
 });
